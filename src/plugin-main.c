fb98497bd647da25dad681598f3634ffff941ec8
Timon Michel <ich.bin@ein.dev>
2022-12-26 17:56:36 +0100

    Update plugin-main.c

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -19,6 +19,7 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <obs-module.h>
 
 #include "plugin-macros.generated.h"
+#include "my_source.c"
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
